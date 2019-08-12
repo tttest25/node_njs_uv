d42247ccaa759a62ac7f432f27b76d5147369737
Ilya Pahov <melnikov-ea@gorodperm.ru>
2019-08-13 02:25:33 +0500

    add krb.conf and fix auth

--- modules/myexpressauth/index.js
+++ modules/myexpressauth/index.js
@@ -17,53 +17,58 @@ class SimpleKerberosError extends NestedError {
     }
 }
 
-let kbServer=null;
+let kbServer = null;
 
-const isWin = (process.platform)==='win32';
+const isWin = (process.platform) === 'win32';
 if (isWin) {
-    module.exports.Kerberos = {username:'Eugen'}; 
+    module.exports.Kerberos = {
+        username: 'Eugen'
+    };
     console.log('Windows');
 } else {
     console.log('Linux');
 }
 
-    const kerberos= require('../kerberos');
+const kerberos = require('../kerberos');
 
-    console.log(`---- Kerberos STARTING SERVER ----`);
-    kerberos.principalDetails('HTTP', "sm.gorodperm.ru")
-        .then((details) => {
-            console.log("Principal:", details);
-        })
-        .catch(error => {
-            console.log("Failed to read principal", error);
-            throw new Error(`Failed to read principal ${error}`);
-        });
+console.log(`---- Kerberos STARTING SERVER ----`);
+kerberos.principalDetails('HTTP', "sm.gorodperm.ru")
+    .then((details) => {
+        console.log("Principal:", details);
+    })
+    .catch(error => {
+        console.log("Failed to read principal", error);
+        throw new Error(`Failed to read principal ${error}`);
+    });
 
-    console.log(`Init kerberos`);
-    kerberos.initializeServer("HTTP@sm.gorodperm.ru")
-        .then((server) => {
-            kbServer = server;
-            module.exports.kbServer=kbServer;
-            console.log(`Kerberos server initialized:`, server);
-        })
-        .catch((error) => {
-            throw new Error(`Failed kerberos initializeServer ${error}`);
-        });
+console.log(`Init kerberos`);
+kerberos.initializeServer("HTTP@sm.gorodperm.ru")
+    .then((server) => {
+        kbServer = server;
+        module.exports.kbServer = kbServer;
+        console.log(`Kerberos server initialized:`, server);
+    })
+    .catch((error) => {
+        throw new Error(`Failed kerberos initializeServer ${error}`);
+    });
 
 
-    
 
-    module.exports.Kerberos = kerberos; 
+
+module.exports.Kerberos = kerberos;
 
 
 async function simpleKerberos(token) {
     kbServer.step(token)
         .then(serverResponse => {
-            console.log('-------3. Kerberos answer %o', kbServer);
+            console.log('-------3. Kerberos answer %o', {
+                kbServer,
+                serverResponse
+            });
             res.setHeader('WWW-Authenticate', 'Negotiate ' + kbServer.response);
             if (kbServer.contextComplete && kbServer.username) {
                 console.log('-----------4.  Auth ok', kbServer.contextComplete);
-                kbServer.step('YIIIIQYGKwYBBQUCo').then(data => console.log('----------reset status %o ', data)).catch(err => console.log('----------catch reset status %o ', err));
+                kbServer.step('YIIIIQYGKwYBBQUCo').then(data => console.log('----------reset status %o ', data)).catch(err => console.error('----------catch reset status %o ', err));
                 return `${kbServer.username}`;
             } else {
                 kbServer.step('').catch(err => {
@@ -71,19 +76,19 @@ async function simpleKerberos(token) {
                 });
             }
         }).catch(err => {
-            console.log('----------finish err %o  kbServer %o', err, kbServer);
-            console.trace("Error")
+            // console.log('----------finish err %o  kbServer %o', err, kbServer);
+            console.trace('----------finish err %o  kbServer %o', err, kbServer)
         });
 }
 
 
 
 module.exports = () => composable()
-.use(expressAuthNegotiate())
-.use((req, res, next) => {
-    simpleKerberos(req.auth.token)
-    .then(username => {
-        req.auth.username = username;
-        next();
-    }, next);
-});
+    .use(expressAuthNegotiate())
+    .use((req, res, next) => {
+        simpleKerberos(req.auth.token)
+            .then(username => {
+                req.auth.username = username;
+                next();
+            }, next);
+    });
